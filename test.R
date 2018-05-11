8293633a953030dd1e6756ee450e75050c6a8155
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-05-10 22:54:44 -0500

    Removed mixSQP.jl.

--- test.R
+++ test.R
@@ -1,7 +1,7 @@
-library("RcppArmadillo")
 library("Rcpp")
-setwd("~/git/mixsqp")
-convtol = 1e-8; ptol = 1e-10; eps = 1e-8; sptol = 1e-3; maxiter = 100; maxqpiter = 100; verbose = T;
+source("mixSQP.R")
+convtol = 1e-8; ptol = 1e-10; eps = 1e-8; sptol = 1e-3;
+maxiter = 100; maxqpiter = 100; verbose = TRUE;
 sourceCpp("temp.cpp")
 
 
@@ -10,19 +10,23 @@ L = as.matrix(read.table("sample5000x20.txt", header = F)); colnames(L) = NULL
 n = dim(L)[1]; m = dim(L)[2];
 x0 = rep(0,m); x0[c(1,2,m)] = 1; x0 = x0/sum(x0)
 
-system.time(out1 <- mixSQP(L, x0, "none", eps, convtol, sptol, ptol, 0, maxiter, maxqpiter))
+system.time(out1 <- mixSQP(L, x0, "none", eps, convtol, sptol, ptol, 0,
+                           maxiter, maxqpiter))
 
-system.time(out2 <- mixsqp_rcpp(L, x0, convtol, ptol, eps, sptol, maxiter, maxqpiter, verbose))
+system.time(out2 <- mixsqp_rcpp(L, x0, convtol, ptol, eps, sptol, maxiter,
+                                maxqpiter, verbose))
 
 out1$x
 as.vector(out2$x)
 
 # x0 MUST BE "double"
-L = as.matrix(read.table("sample100000x100.txt", header = F)); colnames(L) = NULL
+L = as.matrix(read.table("sample100000x100.txt", header = F));
+colnames(L) = NULL
 n = dim(L)[1]; m = dim(L)[2];
 x0 = rep(0,m); x0[c(1,2,m)] = 1; x0 = x0/sum(x0)
 
-system.time(out1 <- mixSQP(L, x0, "none", eps, convtol, sptol, ptol, 0, maxiter, maxqpiter))
+system.time(out1 <- mixSQP(L, x0, "none", eps, convtol, sptol, ptol,
+                           0, maxiter, maxqpiter))
 
 system.time(out2 <- mixsqp_rcpp(L, x0, convtol, ptol, eps, sptol, maxiter, maxqpiter, verbose))
 
