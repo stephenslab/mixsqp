ecf8d0fabb79120e304b61fe348ae303bc18ee0b
Youngseok Kim <workyoungseok@gmail.com>
2018-05-25 15:31:32 -0500

    check git

---
+++ testdata.R
@@ -0,0 +1,20 @@
+testdata <- function(n,m){
+  
+  temp = floor(n/4)
+  x = c(rnorm(n-3*temp),3*rnorm(temp),5*rnorm(temp),rt(temp,df = 2))
+  
+  smin = 1/10;
+  if (all(x^2 < 1)){
+    smax = 1
+  } else{
+    smax = min(sqrt(max(x^2 - 1)), 500); 
+  }
+  
+  
+  grid = c(0,10^seq(log10(smin),log10(smax),length = m-1))
+  L <- matrix(0,n,m)
+  for (j in 1:m)
+    L[,j] <- dnorm(x,sd = sqrt(1 + grid[j]^2))
+  L = L/apply(L,1,max)
+  return(L)
+}
