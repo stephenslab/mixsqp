6163a60e7e7b4b1132a09031efc1ecc94e275887
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-07-02 16:48:24 -0500

    Added output to mixKWDual giving the value of the objective at the solution.
    Former-commit-id: 6eb61fa0843b66c879d3a60ce838bdbb5201c211

--- R/misc.R
+++ R/misc.R
@@ -1,3 +1,14 @@
+# Compute the value of the objective at x; arguments L and w specify
+# the objective, and "eps" is an additional constant that can be set
+# to a small, positive number (zero by default) to better ensure
+# numerical stability of the optimization.
+mixobjective <- function (L, w, x, eps = 0) {
+ if (any(x < 0))
+   return(Inf)
+ else
+   return(-sum(w * log(drop(L %*% x) + eps)))
+}
+
 # Generates a vector of n points that are equally spaced on the
 # logarithmic scale. Note that x and y should be positive numbers.
 logspace <- function (x, y, n)
