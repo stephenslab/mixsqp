00139805374fba3b6204341e5eadcbba76cec62b
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-07-11 10:09:29 -0500

    Implemented mixobjective helper function in Rcpp code.

--- R/misc.R
+++ R/misc.R
@@ -4,14 +4,14 @@ logspace <- function (x, y, n)
   2^seq(log2(x),log2(y),length = n)
 
 # Compute the value of the objective at x; arguments L and w specify
-# the objective, and "eps" is an additional constant that can be set
-# to a small, positive number (zero by default) to better ensure
-# numerical stability of the optimization.
-mixobjective <- function (L, w, x, eps = 0) {
+# the objective, and e is an additional constant that can be set to a
+# small, positive number (zero by default) to better ensure numerical
+# stability of the optimization.
+mixobjective <- function (L, w, x, e = 0) {
  if (any(x < 0))
    return(Inf)
  else
-   return(-sum(w * log(drop(L %*% x) + eps)))
+   return(-sum(w * log(drop(L %*% x) + e)))
 }
 
 # Verify that the likelihood matrix specifying the optimization
