3d660c8173f5c383f651c8a209795505e9b35109
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-04-01 16:52:20 -0500

    Skipping test with tacks data.
    Former-commit-id: a8da154216a18f73f8e19eccfe8d25d6951e955d

--- tests/testthat/test_mixsqp.R
+++ tests/testthat/test_mixsqp.R
@@ -159,6 +159,7 @@ test_that(paste("mix-SQP returns the same solution regardless whether",
                     
 test_that(paste("mix-SQP gives correct solution for Beckett & Diaconis",
                 "tack rolling example"),{
+  skip("Unclear if this test is appropriate; skipping for now")
   data(tacks)
   L   <- tacks$L
   w   <- tacks$w
