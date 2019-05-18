cfcc5cfeb8f231c9e9c9f6bfcd948f0a4aed1780
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-05-17 22:29:09 -0500

    Revised smashr example for debugging.

--- inst/debug/smashr_example.R
+++ inst/debug/smashr_example.R
@@ -1,4 +1,3 @@
 load("smashr.RData")
+out <- mixsqp(L,x0 = x0,control=list(numiter.em = 0,maxiter.sqp=16))
 
-out <- mixsqp(L,x0 = c(1,1),
-              control = list(numiter.em = 0,maxiter.sqp = 20))
