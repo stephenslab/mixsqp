6f859e39a958e6efd113f0f876bced95959d7aea
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-01-27 11:51:07 -0600

    Set gcFirst = FALSE in system.time calls.

--- R/mixsqp.R
+++ R/mixsqp.R
@@ -425,7 +425,7 @@ mixsqp <- function (L, w = rep(1,nrow(L)), x0 = rep(1,ncol(L)),
   
   # Print a brief summary of the analysis, if requested.
   if (verbose) {
-    cat(sprintf("Running mix-SQP algorithm 0.3-15 on %d x %d matrix\n",n,m))
+    cat(sprintf("Running mix-SQP algorithm 0.3-16 on %d x %d matrix\n",n,m))
     cat(sprintf("convergence tol. (SQP):     %0.1e\n",convtol.sqp))
     cat(sprintf("conv. tol. (active-set):    %0.1e\n",convtol.activeset))
     cat(sprintf("zero threshold (solution):  %0.1e\n",zero.threshold.solution))
@@ -447,7 +447,7 @@ mixsqp <- function (L, w = rep(1,nrow(L)), x0 = rep(1,ncol(L)),
   if (tol.svd > 0 & m > 4) {
     if (verbose)
       cat(sprintf("Computing SVD of %d x %d matrix.\n",n,m))
-    timing <- system.time(out <- tsvd(L,tol.svd))
+    timing <- system.time(out <- tsvd(L,tol.svd),gcFirst = FALSE)
     if (is.null(out)) {
       if (verbose)
         cat("Matrix is not low-rank; falling back to full matrix.\n")
@@ -509,7 +509,7 @@ mixsqp <- function (L, w = rep(1,nrow(L)), x0 = rep(1,ncol(L)),
                      suffdecr.linesearch,stepsizereduce,minstepsize,
                      identity.contrib.increase,eps,maxiter.sqp,
                      maxiter.activeset,verbose)
-  })
+  },gcFirst = FALSE)
   if (verbose)
     cat(sprintf("Optimization took %0.2f seconds.\n",timing["elapsed"]))
   
